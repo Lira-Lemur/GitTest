9de80375816d3e055bf0a6df8aaf79da79b6ca61
eparpalac <elviraparpalac@gmail.com>
2017-08-03 14:37:49 +0300

    First Commit

--- app/src/main/java/com/example/eparpalac/gittest/TestClass.java
+++ app/src/main/java/com/example/eparpalac/gittest/TestClass.java
@@ -1,5 +1,14 @@
 package com.example.eparpalac.gittest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class TestClass {
+public class TestClass extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAA");
+    }
 }
